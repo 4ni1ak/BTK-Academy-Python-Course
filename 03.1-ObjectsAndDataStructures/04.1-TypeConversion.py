439be0af0aa98728a4ae06f15d3951240937e0d8
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-06-16 01:15:18 +0300

    Folder is organized

--- 03.1-ObjectsAndDataStructures/04.1-TypeConversion.py
+++
@@ -1,4 +0,0 @@
-x=input('1.number ==>' )
-y=input('2.number ==>' )
-print(type(x))
-sum= int
---
+++ Advanced Python Programming From Zero/03.1-ObjectsAndDataStructures/04.1-TypeConversion.py
@@ -0,0 +1,37 @@
+
+"""
+x=input('1.number ==>' )
+y=input('2.number ==>' )
+print(type(x))
+sum= int(x)+int (y)
+print(sum)
+"""
+
+x=5
+y=2.5
+name ='Anil'
+isOnline=True
+print(type(x))
+print(type(y))
+print(type(name))
+print(type(isOnline))
+
+#x=float(x)
+#print(x)
+#print(type(x))
+
+y = int (y)
+print(y)
+print(type(y))
+result = x + y
+print (result)
+print(type(result))
+
+isOnline=str(isOnline)
+print(isOnline)
+print(type(isOnline))
+
+isOnline=int(isOnline)
+print(isOnline)
+print(type(isOnline))
+
