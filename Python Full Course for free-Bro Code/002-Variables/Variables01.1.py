2cb9e7f9ebfc2d21f36851ca578ef247d862e703
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-06-17 00:46:06 +0300

    5...10 example folders uploaded

---
+++ Python Full Course for free-Bro Code/002-Variables/Variables01.1.py
@@ -0,0 +1,15 @@
+#first_name = "Anil"
+#last_name = "Akpinar"
+#full_name = first_name +" "+ last_name
+#print("Hello "+full_name)
+#print(type(name))
+age = 19
+age += 1
+#print("Your age is :"+str(age))
+print(type(age))
+height=162.5
+print("Your height is: "+str(height)+"cm")
+#print(type(height))
+human = True
+print("Are you a human: "+str(human))
+#print(type (human))
---
+++ Python Full Course for free-Bro Code/004-PythonStringMethods/PythonStringMethods01.1.py
@@ -0,0 +1,12 @@
+name = "Anil"
+
+print(len(name))
+print(name.find("i"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("i"))
+print(name.replace("i","n"))
+print(name*3)
